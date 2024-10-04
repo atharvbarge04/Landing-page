d9a36c70c82290bf2393054b529086e2bfd35741
unknown <atharvbargepatil1800@gmail.com>
2024-10-04 12:46:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+<body>
+
+  
+  <script>
+    let menu = document.querySelector('nav');
+    let menuBtn = document.querySelector('.menu-btn');
+    let closeBtn = document.querySelector('.close-btn');
+  </script>
+</body>
